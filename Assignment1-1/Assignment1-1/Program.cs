9f4f1db2ed231cf54d9461cefff0e146a9330c54
neeraj <neeraj.potlapalli@cognine.com>
2023-05-23 16:35:12 +0530

    assignment1

---
+++ Assignment1-1/Assignment1-1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string string1 = Console.ReadLine();
+            string string2 = Console.ReadLine();
+            int i = 0;
+            int j = string2.Length;
+            int counter = 0;
+            for (i = 0; i < string1.Length - j; i++)
+            {
+                if (string1.Substring(i, j) == string2)
+                {
+                    counter++;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
