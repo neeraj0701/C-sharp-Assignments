66d0379b6204f7ef7b4c4fd936654cdf1d6b0b9f
neeraj0701 <neeraj.potlaplli@cognine.com>
2023-05-23 23:39:42 +0530

    changes done in index method

--- Assignment1-1/Assignment1-1/substrings.cs
+++ Assignment1-1/Assignment1-1/substrings.cs
@@ -2,29 +2,46 @@
 using System.Reflection;
 using Program1;
 
-    public class SubStrings
+public class SubStrings
+{
+    public int CountOfSubStrings(string string1, string string2)
     {
-        public int CountOfSubStrings(string string1,string string2)
+        int counter = 0;
+        if (string1.Length == string2.Length && string1 == string2)
         {
-            int counter = 0;
-            for (int i = 0,j=string2.Length; i < string1.Length - j; i++)
-            {
-                if (string1.Substring(i, j) == string2)
-                {
-                    counter++;
-                }
-            }
-            return counter;
+            counter++;
         }
-        public void IndexesOfSubStrings(string string1,string string2)
+        for (int i = 0, j = string2.Length; i < string1.Length - (j - 1); i++)
         {
-            for (int i = 0, j = string2.Length; i < string1.Length - j; i++)
+            if (string1.Substring(i, j) == string2)
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public void IndexesOfSubStrings(string string1, string string2)
+    {
+        if (string1.Length == string2.Length)
+        {
+            if(string1 == string2)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                Console.WriteLine(-1);
+            }
+        }
+        else
+        {
+            for (int i = 0, j = string2.Length; i < string1.Length - (j - 1); i++)
             {
                 if (string1.Substring(i, j) == string2)
                 {
-                Console.WriteLine(i);
+                    Console.Write(i + " ");
                 }
             }
         }
     }
-
+}
