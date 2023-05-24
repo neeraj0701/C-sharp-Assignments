2503fe405e25e8cb5ac4c1907f5471029e8940fb
neeraj0701 <neeraj.potlaplli@cognine.com>
2023-05-24 23:17:30 +0530

    added files

---
+++ Assignment1-2/Assignment1-2/UtilityFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Program2;
+
+public class UtilityFunctions
+{
+    public int LengthOfString(string chars)
+    {
+        int counter = 0;    
+        foreach (char c in chars)
+        {
+            counter++;
+        }
+
+        return counter;
+    }
+}
+
