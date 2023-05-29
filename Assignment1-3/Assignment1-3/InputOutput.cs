db8b805cb8ccb9aecc1e65134c76c742ec8c3a91
neeraj <neeraj.potlapalli@cognine.com>
2023-05-29 09:32:07 +0530

    commit

---
+++ Assignment1-3/Assignment1-3/InputOutput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Program3;
+
+public class InputOutput
+{
+    public float Input1()
+    {
+        Console.WriteLine("enter the first float value");
+        return float.Parse(Console.ReadLine());
+    }
+
+    public float Input2()
+    {
+        Console.WriteLine("enter the second float value");
+        return float.Parse(Console.ReadLine());
+    }
+}
+
---
+++ Assignment1-3/Assignment1-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Program3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            InputOutput inputOutput = new InputOutput();
+            //float number1 = inputOutput.Input1();
+            //float number2 = inputOutput.Input2();   
+            BinaryAddition binaryAddition = new BinaryAddition();
+            //Console.WriteLine(binaryAddition.FloatToBinary(number1));
+            //Console.WriteLine(binaryAddition.FloatToBinary(number2));
+            float n = 50.5f;
+            string a = "";
+            while (n >= 1)
+            {
+                a = (n % 2) + a;
+                n = n / 2;
+            }
+            Console.Write(a);
+
+            //Console.WriteLine(0.8*0.1);
+        }
+    }
+}
